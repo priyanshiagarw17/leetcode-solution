2bb418d10f79d114109eff64ffa798e6ac56e16d
priyanshiagarw17 <priyanshi.agarwal_cs22@gla.ac.in>
2024-03-08 00:23:19 +0530

    solve remove element Solution.java for practice

---
+++ src/main/java/removeelement/Solution.java
@@ -0,0 +1,18 @@
+package removeelement;
+
+public class Solution {
+
+        public int removeElement(int[] nums, int val) {
+            int i = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] != val) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    i++;
+                }
+            }
+            return i;
+        }
+
+}
