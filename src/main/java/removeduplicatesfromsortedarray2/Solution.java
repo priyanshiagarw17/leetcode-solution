18810931c020d395bc4906e809fb847d6ed54f31
priyanshiagarw17 <priyanshi.agarwal_cs22@gla.ac.in>
2024-03-19 18:43:41 +0530

    solve remove duplicates from sorted array 2 Solution.java for practice

---
+++ src/main/java/removeduplicatesfromsortedarray2/Solution.java
@@ -0,0 +1,14 @@
+package removeduplicatesfromsortedarray2;
+
+public class Solution {
+        public int removeDuplicates(int[] nums) {
+            int j=1;
+            for(int i=2;i<nums.length;i++)
+                if(nums[i]!=nums[j-1]){
+                    j++;
+                    nums[j]=nums[i];
+                }
+            return j+1;
+        }
+
+}
