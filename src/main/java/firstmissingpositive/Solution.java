e83d5d077a14befb3c2b54a42ff2871ddd841a0f
priyanshiagarw17 <priyanshi.agarwal_cs22@gla.ac.in>
2024-03-19 11:32:41 +0530

    solve first missing positive Solution.java for practice

---
+++ src/main/java/firstmissingpositive/Solution.java
@@ -0,0 +1,17 @@
+package firstmissingpositive;
+
+import java.util.Arrays;
+
+public class Solution {
+
+        public int firstMissingPositive(int[] nums) {
+            Arrays.sort(nums);
+            int a=1;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]>0 && a==nums[i])
+                    a++;
+            }
+            return a;
+        }
+    }
+
